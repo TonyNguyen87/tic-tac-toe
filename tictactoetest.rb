801cc8a7587e9c2ac1978ae6bf46c384c593a0b5
Tony Nguyen <tonyviet87@gmail.com>
2016-02-29 15:22:39 -0500

    changing game to player vs cpu

--- tictactoetest.rb
+++ tictactoetest.rb
@@ -3,15 +3,16 @@ require "pry"
 
 board = [1,2,3,4,5,6,7,8,9]
 
-MAX_TURNS = 9
+WINS = [[1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]]
 
-# def display_board(board)
-# 	puts	
-#   3.times do |row|
-#     puts board[row * 3, 3].join(" | ")
-#   end
-#   puts
-# end
+MAX_TURNS = 9
 
 def display_board(board)
   puts "
@@ -31,11 +32,17 @@ def greeting
 	puts "PLAYER ONE GOES FIRST"
 end
 
-def take_turn(turn_count)
-	puts "Enter a number from the board to place your piece"
-	number = gets.chomp.to_i 
-	until (1..9).include?(number)
-    	puts "Please choose a number."
+
+def available_moves(board)
+  board.select { |piece| piece.is_a?(Fixnum) }
+end
+
+def take_turn(board, current_player, turn_count)
+	display_board(board)
+	puts "Player #{current_player} Enter a number from the board to place your piece"
+	number = gets.chomp.to_i
+	until available_moves(board).include?(number)
+    	puts "Please choose an availble number from the board."
     	number = gets.chomp.to_i
 	end
 	number  
@@ -49,44 +56,28 @@ def draw?(turn_count)
 	turns_left(turn_count).zero?
 end
 
-def win?(current_positions)
-	if current_positions == [1,2,3].to_set
-		true
-	elsif current_positions == [4,5,6].to_set	
-		true
-	elsif current_positions == [7,8,9].to_set
-		true
-	elsif current_positions == [1,4,7].to_set
-		true
-	elsif current_positions == [2,5,8].to_set
-		true
-	elsif current_positions == [3,6,9].to_set
-		true
-	elsif current_positions == [1,5,9].to_set
-		true	
-	elsif current_positions == [3,5,7].to_set
-		true
-	else
-		false
-	end
+def win?(board, current_positions)
+  WINS.any? do |x, y, z|
+    board[x - 1] == board[y - 1] && board[y - 1] == board[z - 1]
+  end
 end
 
-# def win?(current_positions) 
-# # are any of the current player position included in the win?
+def game_over(board, current_positions, turn_count)
+	win?(board, current_positions) || draw?(turn_count)
+end
 
-# winnable_combos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]].to_set
+def place_piece(choice, board, current_player)
+	board.map! do |number|
+		if number == choice
+		  current_player
+		else
+		  number
+		end
+	end	
+end
 
-# 	winning_set = winnable_combos
-# 	winning_set.map! do |win|  
-# 	  if win == current_positions
-# 	  	true
-# 	  end
-# 	end
-# 	false
-# end
-
-def game_over(current_positions, turn_count)
-	win?(current_positions) || draw?(turn_count)
+def cpu_turn(board)
+	available_moves(board).sample.to_i
 end
 
 def tictactoe(board)
@@ -96,58 +87,38 @@ def tictactoe(board)
 	p1_progress = Set.new
 	p2_progress = Set.new
 	greeting
-	current_player = p1
+	current_player = 1
 	current_positions = p1_progress
-	until game_over(current_positions, turn_count)
-		display_board(board)
-		choice = take_turn(turn_count)
-		board.map! do |number|
-			if number == choice
-			  current_player
-			else
-			  number
-			end
-		end
-		current_positions.add(choice)
-			if current_player == p1
-      			current_player = p2
-      			current_positions = p2_progress
-    		else
-      			current_player = p1
-      			current_positions = p1_progress
-      		end
+	until game_over(board, current_positions, turn_count)
+			choice = take_turn(board, current_player, turn_count)
+			place_piece(choice, board, current_player)
+			current_positions.add(choice)
+		else current_player == 1
+      		current_player = p2
+      		current_positions = p2_progress
+      		puts "Its the CPUs turn."
+      		choice = cpu_turn(board)
+      		place_piece(board, current_positions, turn_count)
+      		current_positions.add(choice)
+      	end
       	turn_count += 1
 	end
-	postmortem(current_positions)
+	if current_player == p1
+	  	current_player = p2
+    else
+     	current_player = p1
+    end	
+	postmortem(board, current_positions, current_player)
 end
 
-
-
-def postmortem(current_positions)
-	if win?(current_positions)
-		puts "CONGRATULATIONS YOU WIN"
+def postmortem(board, current_positions, current_player)
+	if win?(board, current_positions)
+		display_board(board)
+		puts "CONGRATULATIONS YOU WIN #{current_player}"
 	else
+		display_board(board)
 		puts "ITS A DRAW"
 	end
 end
 
 tictactoe(board)
-
-# display_board(board) ##always have to pass arguement
-
-
-## DATA IN THE SYSTEM
-# * board
-# * player1 (name/piece), player2
-# * win conditions
-# * current_player => player1, player2
-
-# board = {
-#   1 => 1,
-#   2 => 2,
-#   3 => 3
-# }
-
-# board[0][1]
-# board[1][2]
-
